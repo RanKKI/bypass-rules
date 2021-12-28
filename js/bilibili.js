10540aa800b57b5fe39bca7442012db67c9c7400
RanKKI <youloveonlymeh@gmail.com>
2021-12-28 15:07:55 +0800

    fix: edge

--- js/bilibili.js
+++ js/bilibili.js
@@ -22,7 +22,7 @@ delete obj.data.vip_section_v2
 delete obj.data.live_tip
 let sections = obj.data.sections_v2
 if (Array.isArray(sections)) {
-    const disabledTitles = ["推荐服务", "创作中心", "推荐服务"]
+    const disabledTitles = ["创作中心", "推荐服务"]
     const disabledIds = [408, 409]
     sections = sections.filter(val => {
         return disabledTitles.indexOf(val.title) == -1
