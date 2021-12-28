bfe4851b921281b892f6a3520f6f1f6f21c4e58f
RanKKI <youloveonlymeh@gmail.com>
2021-12-28 14:44:55 +0800

    feat: use script from online

--- js/bilibili.js
+++ js/bilibili.js
@@ -1,4 +1,7 @@
 function removeWhenMatch(arr, met) {
+    if (!arr) {
+        return
+    }
     if (Array.isArray(arr)) {
         let i = 0
         let found = false
@@ -13,18 +16,19 @@ function removeWhenMatch(arr, met) {
     }
 }
 
-var obj = JSON.parse($response.body);
-try {
-    if (obj.data) {
-        delete obj.data.vip_section
-        delete obj.data.vip_section_right
-        delete obj.data.vip_section_v2
-        var sections = obj.data.sections_v2
-        removeWhenMatch(sections, (item) => {
-            return item.title == "推荐服务"
-        })
-    }
-} catch (err) {
-    console.log("[bilibili.js]", (err && err.message) || err)
+if (obj.data) {
+    delete obj.data.vip_section
+    delete obj.data.vip_section_right
+    delete obj.data.vip_section_v2
+    delete obj.data.live_tip
+    removeWhenMatch(obj.data.sections_v2, (item) => {
+        return item.title == "推荐服务"
+    })
+    removeWhenMatch(obj.data.sections_v2, (item) => {
+        return item.title == "创作中心"
+    })
+    removeWhenMatch(obj.data.sections_v2, (item) => {
+        return item.title == "推荐服务"
+    })
 }
 $done({ body: JSON.stringify(obj) }); 
